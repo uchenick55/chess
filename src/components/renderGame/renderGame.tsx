4c48a212b975a856b0ac037cfef64621f80e3577
uchenick55 <uchenick55@yandex.ru>
2024-01-01 11:00:16 +0300

    вывел шахматное поле через inline block

--- src/components/renderGame/renderGame.tsx
+++ src/components/renderGame/renderGame.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import {AppDispatch, GlobalStateType} from "../../redux/store-redux";
 import {useDispatch, useSelector} from "react-redux";
 import {FiedlType} from "../common/types/commonTypes";
+import classes from '../renderGame/renderGame.module.css'
 
 const RenderGame: React.FC = (() => {
     const field: FiedlType = useSelector( (state: GlobalStateType) => state.chess.field )
     const dispatch = useDispatch<AppDispatch>();
     console.log(field)
     return <div>
-            555
+        {field.map((f, i)=>{
+            console.log(f)
+            return <div key={i}>
+                {f.map((cell, indCell)=>{
+                    console.log(cell)
+                    return <div className={ cell.cellColor==="white"? classes.cellWhite: classes.cellBlack}>
+
+                    </div>
+                })}
+
+            </div>
+        })}
     </div>
 })
 export default  RenderGame;
